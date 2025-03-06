33bb3d630a1c133618e9629de59463e5ea83b0e8
Yotam Frid <yotam.frid@explorium.ai>
2025-03-06 15:46:51 +0200

    rename src folder

---
+++ src/explorium_mcp_server/__init__.py
@@ -0,0 +1,3 @@
+"""MCP server for interacting with the Explorium API."""
+
+__version__ = "0.1.0"
--- src/explorium_mcp_server/server.py
+++ src/explorium_mcp_server/server.py
@@ -2,3 +2,11 @@ import tools_businesses
 import tools_prospects
 
 from _shared import mcp
+
+
+def main():
+    mcp.run()
+
+
+if __name__ == "__main__":
+    main()
