0821bb0c646243655d96def5431aa52ddf067a2b
shahar.luftig <shahar.luftig@explorium.ai>
2025-04-16 14:16:42 +0300

    feat: remove country autocomplete

--- src/explorium_mcp_server/models/enum_types.py
+++ src/explorium_mcp_server/models/enum_types.py
@@ -6,7 +6,6 @@ from pydantic import Field
 AutocompleteType = Annotated[
     Literal[
         "country",
-        "country_code",
         "region_country_code",
         "google_category",
         "naics_category",
